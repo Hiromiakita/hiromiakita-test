704525199805eeacea35275703b6216eb5a608a0
hiromiakitao@gmial.com <hiromiakitao@gmial.com>
2022-01-03 14:44:50 -0600

    style changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"> 
-        <p>Yo soy Hiromi Akita</p>
+        <h1 className='title'>Hiromi Akita</h1>
+        <p className='subtitle'>Work in progress, but we can be friends on LinkedIn :&#41;</p>
+        <nav>
+          <a href='https://www.linkedin.com/in/hiromiakita/' target='_blank' rel="noopener noreferrer" >
+            <FontAwesomeIcon icon={faLinkedin} color="#0077b5" size="lg"/>
+          </a>
+        </nav>
       </header>
     </div>
   );
